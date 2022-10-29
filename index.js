7002054ee83190c910b75fd8c0f2f2d4497dcd2e
TCO46 <minecraftduc12@gmail.com>
2022-10-29 20:54:58 +0700

    Add scroll up function

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const getData = async () => {
     
     try {
     //Fetching data from api
-    res = await fetch(`https://spotify81.p.rapidapi.com/search?q=${search}&type=tracks&offset=0&limit=10&numberOfTopResults=5`, options)
+    res = await fetch(`https://spotify81.p.rapidapi.com/search?q=${search}&type=tracks&offset=0&limit=20&numberOfTopResults=5`, options)
     
     data = await res.json()
 
@@ -56,3 +56,21 @@ function reload() {
     document.location.reload()
 }
 
+let mybutton = document.getElementById("scroolUp");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
