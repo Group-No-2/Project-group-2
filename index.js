1fee41df3f987a750c17c530571bde62c651c0ad
TCO46 <minecraftduc12@gmail.com>
2022-10-30 16:15:58 +0700

    Change function name

--- index.js
+++ index.js
@@ -56,20 +56,18 @@ function reload() {
     document.location.reload()
 }
 
-let mybutton = document.getElementById("scrollUp");
+let srollBtn = document.getElementById("scrollUp");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    srollBtn.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    srollBtn.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
